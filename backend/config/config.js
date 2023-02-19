109ed946b83b72ffb3a581b30cc79cd2c2827e5f
Your Name <you@example.com>
2023-02-19 10:39:42 -0800

    added codes

--- backend/config/config.js
+++ backend/config/config.js
@@ -2,24 +2,24 @@ require('dotenv').config()
 
 module.exports = {
   "development": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE,
+    "username": process.env.postgres,
+    "password": process.env.postgres_,
+    "database": process.env.rest_rant_auth,
     "host": "127.0.0.1",
     "dialect": "postgres"
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
+    "username": "postgres",
+    "password": "postgres_",
+    "database": "rest_rant_auth",
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "postgres"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
+    "username": "postgres",
+    "password": "postgres_",
+    "database": "rest_rant_auth",
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "postgres"
   }
 }
