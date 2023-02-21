c0e7ed4bbd506e2f09b1d0f7114dc4897c6189fe
Your Name <you@example.com>
2023-02-20 21:24:05 -0800

    made several changes on codes for assignment

--- backend/config/config.js
+++ backend/config/config.js
@@ -2,9 +2,9 @@ require('dotenv').config()
 
 module.exports = {
   "development": {
-    "username": process.env.postgres,
-    "password": process.env.postgres_,
-    "database": process.env.rest_rant_auth,
+    "username": 'postgres',
+    "password": 'postgres_',
+    "database": 'rest_rant_auth',
     "host": "127.0.0.1",
     "dialect": "postgres"
   },
--- backend/controllers/places.js
+++ backend/controllers/places.js
@@ -135,4 +135,4 @@ router.delete('/:placeId/comments/:commentId', async (req, res) => {
 })
 
 
-module.exports = router
+module.exports = router;
