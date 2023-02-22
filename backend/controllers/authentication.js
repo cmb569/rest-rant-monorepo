6ba14df7d6a33c485a56b8b8f802e986edd13e53
Your Name <you@example.com>
2023-02-21 19:47:50 -0800

    Added code

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -17,9 +17,26 @@ router.post('/', async (req, res) => {
             message: `Could not find a user with the provided username and password` 
         })
     } else {
+        req.session.userid = user.userId
         res.json({ user })
     }
 })
+
+
+router.get('/profile', async (req, res) => {
+    try {
+        let user = await User.findOne({
+            where: {
+                userId: req.session.userId
+            }
+        })
+        res.json(user)
+    } catch {
+        res.json(null)
+    }
+})
+
+
 
 
 
