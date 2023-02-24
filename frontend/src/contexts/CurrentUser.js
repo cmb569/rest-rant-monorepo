c5e250a48d250c8885fcd5de738fcd1c9e92fa06
Your Name <you@example.com>
2023-02-23 18:54:13 -0800

    added more codes

--- frontend/src/contexts/CurrentUser.js
+++ frontend/src/contexts/CurrentUser.js
@@ -6,16 +6,14 @@ export const CurrentUser = createContext()
 function CurrentUserProvider({ children }) {
 
     const [currentUser, setCurrentUser] = useState(null)
-
     useEffect(() => {
-const getLoggedInUser = async () => {
+
+        const getLoggedInUser = async () => {
             let response = await fetch('http://localhost:5000/authentication/profile', {
-                headers: {
-                    'Authorization': `Bearer ${localStorage.getItem('token')}`
-                }
+                credentials: 'include'
             })
-let user = await response.json()
-setCurrentUser(user)
+            let user = await response.json()
+            setCurrentUser(user)
         }
         getLoggedInUser()
     }, [])
